44541a33b5bc442b96e04f70b4c9dd4acb9b7622
cescobedosaavedra <chrcar_e@hotmail.com>
2015-09-21 22:19:14 -0500

    Registro

---
+++ principal/Bandeja.aspx.cs
@@ -0,0 +1,37 @@
+﻿
+using principal.RegistroTicketsServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace principal
+{
+    public partial class Bandeja : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_listar_Click(object sender, EventArgs e)
+        {
+            
+            IGuardaTickets enlace = new GuardaTicketsClient();
+            //int numeroTicket = int.Parse(txt_numeroTicket.Text);
+            GuardaTicket campo = new GuardaTicket();
+
+            campo.CorreoEmpleado = txt_numeroTicket.Text;
+
+            int t = '1';
+            //txt_numeroTicket.Text = campo.CorreoEmpleado;
+            grid_tickets.DataSource = enlace.ObtenerTicket(t);
+            
+            //grid_tickets.DataBind();
+
+
+        }
+    }
+}
